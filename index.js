97b065fb240afd973c771ca4dd74750094318a3c
Daniel Leichus <daniel.leichus@appian.com>
2018-04-10 18:34:17 -0400

    added view counter using express-session and mongo

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
+const session = require('express-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const MongoStore = require('connect-mongo')(session);
 require('./models/user');
 require('./services/passport');
 
@@ -11,13 +12,27 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 2 * 24 * 60 * 60
+    }),
+    secret: keys.sessionSecret,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000
+    }
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.get('/viewCounter', function(req, res, next) {
+  req.session.views = (req.session.views || 0) + 1;
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<p>views: ' + req.session.views + '</p>')
+  res.end()
+})
+
 
 require('./routes/authRoutes')(app);
 
